8b26699434e9b670963e80f6a4a92652d6c1a5b0
MikhailHusyev <mikhailhusyev@gmail.com>
2020-10-19 12:24:49 -0500

    Added private and converted to scss

---
+++ covid-room-tracker/src/app/private/side_panel/sidepanel.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'side-panel',
+  templateUrl: 'sidepanel.component.html',
+  styleUrls: ['sidepanel.component.scss'],
+})
+export class SidePanelComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
