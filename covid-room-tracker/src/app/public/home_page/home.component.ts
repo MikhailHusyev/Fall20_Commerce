9eb151372f6d8daec1b8d9724c03da892566c68f
MikhailHusyev <mikhailhusyev@gmail.com>
2020-11-16 00:42:46 -0600

    Revert "Frontend update to the home page pt 3"
    This reverts commit e75df2f781f2f65694482051c593065360d9788e.

--- covid-room-tracker/src/app/public/home_page/home.component.ts
+++ covid-room-tracker/src/app/public/home_page/home.component.ts
@@ -1,78 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { fade } from './animations';
 
 
 @Component({
   selector: 'home-page',
   templateUrl: 'home.component.html',
-  styleUrls: ['home.component.scss'],
-  animations: fade
+  styleUrls: ['home.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  choice = 2;
-  state ='in';
-  counter = 0;
-  enableAnimation = false;
-  imageSource = '';
-  imgSrc1 = '../../../assets/Homepage/second_portion.jpg';
-  imgSrc2 = '../../../assets/Homepage/stayinformed.jpg';
-  imgSrc3 = '../../../assets/Homepage/managePicture.jpeg';
-
   constructor() {}
 
-  ngOnInit() {
-    this.imageSource = this.imgSrc1;
-  }
-
-  onClick(){
-    this.enableAnimation = true;
-    this.counter = 0;
-    this.toggleState();
-    this.choice = 1;
-  }
-  onClick2(){
-    this.enableAnimation = true;
-    this.counter = 0;
-    this.toggleState();
-    this.choice = 2;
-  }
-  onClick3(){
-    this.enableAnimation = true;
-    this.counter = 0;
-    this.toggleState();
-    this.choice = 3;
-  }
-
-
-
-  toggleImg(){
-    if (this.choice === 1){
-      this.imageSource = this.imgSrc1;
-      this.choice = 2;
-    } else if (this.choice === 2){
-      this.imageSource = this.imgSrc2;
-      this.choice = 3;
-    } else {
-      this.imageSource = this.imgSrc3;
-      this.choice = 1;
-    }
-  }
-
-  onDone($event){
-    if(this.enableAnimation) {
-      if (this.counter === 1) {
-        this.toggleImg();
-      }
-      this.toggleState();
-    }
-  }
-
-
-  toggleState() {
-    if (this.counter < 2) {
-      this.state = this.state === 'in' ? 'out' : 'in';
-      this.counter++;
-    }
-  }
-
+  ngOnInit() {}
 }
