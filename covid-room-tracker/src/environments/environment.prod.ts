86fcea5c501b9cf4cc85910944e684fee6d359b6
MikhailHusyev <mikhailhusyev@gmail.com>
2020-11-29 00:37:38 -0600

    Updating For Heroku

--- covid-room-tracker/src/environments/environment.prod.ts
+++ covid-room-tracker/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: false,
+  clientId: '59b7132a-f776-48e0-a86e-7120b66d041a',
+  authority: 'https://login.microsoftonline.com/common',
+  redirectUrl: 'http://localhost:4200/profile',
 };
