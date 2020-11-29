ffb7ec80e816b20daf6adfd6a5d49b409575e733
MikhailHusyev <mikhailhusyev@gmail.com>
2020-11-29 01:15:42 -0600

    Prod = True

--- covid-room-tracker/src/environments/environment.prod.ts
+++ covid-room-tracker/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   clientId: '59b7132a-f776-48e0-a86e-7120b66d041a',
   authority: 'https://login.microsoftonline.com/common',
   redirectUrl: 'https://covid-room-tracker.herokuapp.com/profile',
