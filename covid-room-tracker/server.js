30dc4da436839addf76c6d3b9219a39166142928
MikhailHusyev <mikhailhusyev@gmail.com>
2020-11-29 00:51:03 -0600

    Updating For Heroku

--- covid-room-tracker/server.js
+++ covid-room-tracker/server.js
@@ -4,11 +4,11 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + "/src"));
+app.use(express.static(__dirname + "/dist/covid-room-tracker"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/angularapp/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/covid-room-tracker/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
