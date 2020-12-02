0075838d8831f90740e3015f5703fc35ecb12197
MikhailHusyev <mikhailhusyev@gmail.com>
2020-12-02 09:56:40 -0600

    Default route

--- Covid-Room-Tracker-API/src/main/java/com/covidroomtracker/CovidRoomTrackerApplication.java
+++ Covid-Room-Tracker-API/src/main/java/com/covidroomtracker/CovidRoomTrackerApplication.java
@@ -2,9 +2,19 @@ package com.covidroomtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;;
 
+@Controller
 @SpringBootApplication
 public class CovidRoomTrackerApplication {
+
+	@RequestMapping("/")
+    @ResponseBody
+    public String home() {
+      return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CovidRoomTrackerApplication.class, args);
