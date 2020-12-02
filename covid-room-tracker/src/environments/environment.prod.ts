2ac5e486d4d9b2abc62664e4a9ecd382f399ce21
MikhailHusyev <mikhailhusyev@gmail.com>
2020-12-02 10:27:00 -0600

    Updating Environment Variables

--- covid-room-tracker/src/environments/environment.prod.ts
+++ covid-room-tracker/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   clientId: '59b7132a-f776-48e0-a86e-7120b66d041a',
   authority: 'https://login.microsoftonline.com/common',
-  redirectUrl: 'https://covid-room-tracker.herokuapp.com/profile',
+  redirectUrl: 'https://covid-room-tracker.herokuapp.com',
   returnUrl: 'https://covid-room-tracker.herokuapp.com',
   apiBaseUrl: 'https://covid-room-tracker-spring-api.herokuapp.com',
 };
